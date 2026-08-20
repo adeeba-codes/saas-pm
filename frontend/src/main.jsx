ea6bead6d767c10ea59102ddfbdb408c2c00408c
adeeba-codes <adeebafaraz8@gmail.com>
2026-08-20 17:47:05 +0530

    Change BrowserRouter to HashRouter in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ window.global = window
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
